06f94099f7fc1cbe380ab39bc56e7cc0e38eb0f2
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2023-11-02 22:27:10 -0300

    introducao de data-binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -12,11 +12,29 @@ export class DataBindingComponent implements OnInit {
   cursoAngular :boolean = true;
   urlImagem = 'https://loremflickr.com/640/360'
 
+  valorAtual: string ='';
+  valorSalvo: any;
+  isMouseOver: boolean = false ;
+
   getValor () {
       return 1;
   }
   getCurtirCurso (){
     return true;
+  }
+
+  botaoClicado() {
+    alert('Botao clicado!');
+  }
+
+  onKeyUp(evento: KeyboardEvent){
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+  }
+  salvarValor(valor: any){
+    this.valorSalvo = valor;
+  }
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
   }
 
   constructor() { }
