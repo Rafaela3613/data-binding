6c6b76470c91d88ca720f0af6d1d1be18cd4d5a2
Rafaela3613 <rafaelarabelobarbosa@outlook.com>
2023-11-04 15:59:28 -0300

    incrementei output-property

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -16,6 +16,8 @@ export class DataBindingComponent implements OnInit {
   valorSalvo: any;
   isMouseOver: boolean = false ;
   nomeDoCurso: string = 'Angular';
+  valorInicial= 15;
+
 
   getValor () {
       return 1;
